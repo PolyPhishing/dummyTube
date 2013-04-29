0f013ff7b4e1f6714979e4b2581d9b067641c87a
Darshan Pandit <darshan12345@gmail.com>
2013-04-29 03:15:44 -0400

    Added the thumbnail Images and removed the background for Suggestion Box

--- routes/index.js
+++ routes/index.js
@@ -13,28 +13,35 @@ exports.registerPhish = function(req, res){
 exports.nothing= function(req, res){
 	console.log('Appropriate Nothing Route is called.');
 	wrapper = new Object();
-	wrapper.comments = [{name: 'UAEchemist2 ', message: 'The quake﻿ happened when i was sitting in my college lab talking with the assistant lab technician when all of a sudden the whole building,glassware and the ground was shaking.'}, {name: 'Elbert kwan', message:'My﻿ mom is there'}, {name: 'alaa sohil ', message:'I almost got ran over by a truck, and the most﻿ horrible car accident ever'},{name:'Waheed Ahmed', message:'i m in abudhabi near﻿ mushrif mall'},{name:'Ali Jericho ', message:'Chuck norris was on vacation in Abu dhabi when﻿ he was suddenly told he cant have alcohol without a license here'}];
-                
-	wrapper.suggestions = [{ videoTitle:'الصور الاولى للزلزال الذي ضرب جنوب إيران', thumbnailSrc:'https://i4.ytimg.com/vi/SaU_a1x1AcA/default.jpg'}, 
-		{ videoTitle:'زلزال إيران وهزات بالخليج دبي وابوظبي والفجيرة والسعودية والبحرين والكويت', thumbnailSrc:'https://i1.ytimg.com/vi/PLtRrWkB094/default.jpg'}, 
-		{ videoTitle:'هزة أرضية في الخليج خروج السكان من مبانيهم في ابوظبي وعمليات أجلاء', thumbnailSrc:'https://i3.ytimg.com/vi/Jlu4SPyjDfY/default.jpg'}, 
-		{ videoTitle:'لحظة وقوع الزلزال', thumbnailSrc:'https://i1.ytimg.com/vi/0WrcBhKLtes/default.jpg'}, 
-		{ videoTitle:'إخلاء مبان في الإمارات بعد الزلزال', thumbnailSrc:'https://i1.ytimg.com/vi/xBGru3yI-Zg/default.jpg'}, 
-		{ videoTitle:'JAPAN The Earthquake 15 Minutes Live-Cam', thumbnailSrc:'https://i3.ytimg.com/vi/jBdvvXyS-r4/default.jpg'}, 
-		{ videoTitle:'زلزال بقوة 8 ريختر يضرب إيران وسقوط أكثر من 40 قتيلاً - YouTube', thumbnailSrc:'https://i4.ytimg.com/vi/37Y6Ew3bCnQ/default.jpg'}, 
-		{ videoTitle:'زلزال يضرب ايران وارتداده يهز الامارات والخليج 16 / 4 / 2013', thumbnailSrc:'https://i3.ytimg.com/vi/rQ2-uMLZC8U/default.jpg'}, 
-		{ videoTitle:'زلزال اليابان | صور مرعبة', thumbnailSrc:'https://i3.ytimg.com/vi/B6_TEbzcgmI/default.jpg'}, 
-		{ videoTitle:'Dancing House', thumbnailSrc:'https://i4.ytimg.com/vi/KI2TOP1MtrE/default.jpg'}, 
-		{ videoTitle:'دبي.. سكان أبراج يغادرونها بعد الهزة', thumbnailSrc:'https://i4.ytimg.com/vi/CYVK_mKYV0U/default.jpg'}, 
-		{ videoTitle:'اعنف زلزال يضرب ايران منذ 40عاما', thumbnailSrc:'https://i2.ytimg.com/vi/5jy0wbyQMq0/default.jpg'}, 
-		{ videoTitle:'زلزال يضرب قارة اسيا بالكامل', thumbnailSrc:'https://i4.ytimg.com/vi/wfNbO291NO0/default.jpg'}, 
-		{ videoTitle:'31 قتيلا على الأقل ومئات الجرحى في زلزال يضرب جنوب إيران', thumbnailSrc:'https://i2.ytimg.com/vi/EcHP5-pW_4M/default.jpg'}, 
-		{ videoTitle:'زلزال بقوة 6.3 قرب بوشهر في ايران يشعر به سكان دولة الإمارات', thumbnailSrc:'https://i2.ytimg.com/vi/ydkf8zOfh2Y/default.jpg'}, 
-		{ videoTitle:'IRANS EARTHQUAKES AND NUCLEAR REACTORS LAUNCHING!', thumbnailSrc:'https://i2.ytimg.com/vi/9NWJ-Hz_3zw/default.jpg'}, 
-		{ videoTitle:'زلزال يضرب إيران ويهز دول الخليج', thumbnailSrc:'https://i4.ytimg.com/vi/kqmIrPec2WA/default.jpg'}, 
-		{ videoTitle:'Iran Earthquake (Magnitude 7,8) Aftermath Footage - April 16, 2013', thumbnailSrc:'https://s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif'}, 
-		{ videoTitle:'الإمارات.. لا خسائر بعد الزلزال', thumbnailSrc:'https://s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif'}, 
-		{ videoTitle:'Mega Earthquake Shakes Iran ', thumbnailSrc:'https://s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif'}] ;
+	wrapper.comments = [{name: 'UAEchemist2 ', message: 'The quake﻿ happened when i was sitting in my college lab talking with the assistant lab technician when all of a sudden the whole building,glassware and the ground was shaking.', image:'../images/user/2EQRv_fji672QT.jpg'}
+		,{name: 'Elbert kwan', message:'My﻿ mom is there', image:'../images/user/49tghUROIh35t.jpg'}
+		,{name:'Waheed Ahmed', message:'i m in abudhabi near﻿ mushrif mall', image:'../images/user/photo.jpg'}
+	 	,{name:'Ali Jericho ', message:'Chuck norris was on vacation in Abu dhabi when﻿ he was suddenly told he cant have alcohol without a license here',image:'../images/user/photo.jpg'}
+		,{name: 'alaa sohil ', message:'I almost got ran over by a truck, and the most﻿ horrible car accident ever', image:'../images/user/4546gFFGe35g.jpg'}
+		];
+         
+     console.log(wrapper)       
+	wrapper.suggestions = [
+		{ videoTitle:'الصور الاولى للزلزال الذي ضرب جنوب إيران', thumbnailSrc:'../images/thumbnails/SaU_a1x1AcA.jpg'}, 
+		{ videoTitle:'زلزال إيران وهزات بالخليج دبي وابوظبي والفجيرة والسعودية والبحرين والكويت', thumbnailSrc:'../images/thumbnails/0WrcBhKLtes.jpg'}, 
+		{ videoTitle:'هزة أرضية في الخليج خروج السكان من مبانيهم في ابوظبي وعمليات أجلاء', thumbnailSrc:'../images/thumbnails/5jy0wbyQMq0.jpg'}, 
+		{ videoTitle:'لحظة وقوع الزلزال', thumbnailSrc:'../images/thumbnails/37Y6Ew3bCnQ.jpg'}, 
+		{ videoTitle:'إخلاء مبان في الإمارات بعد الزلزال', thumbnailSrc:'../images/thumbnails/B6_TEbzcgmI.jpg'}, 
+		{ videoTitle:'JAPAN The Earthquake 15 Minutes Live-Cam', thumbnailSrc:'../images/thumbnails/CYVK_mKYV0U.jpg'}, 
+		{ videoTitle:'زلزال بقوة 8 ريختر يضرب إيران وسقوط أكثر من 40 قتيلاً - YouTube', thumbnailSrc:'../images/thumbnails/D34uMLZC8U.jpg'}, 
+		{ videoTitle:'زلزال يضرب ايران وارتداده يهز الامارات والخليج 16 / 4 / 2013', thumbnailSrc:'../images/thumbnails/Jlu4SPyjDfY.jpg'}, 
+		{ videoTitle:'زلزال اليابان | صور مرعبة', thumbnailSrc:'../images/thumbnails/KI2TOP1MtrE.jpg'}, 
+		{ videoTitle:'Dancing House', thumbnailSrc:'../images/thumbnails/jBdvvXyS-r4.jpg'}, 
+		{ videoTitle:'دبي.. سكان أبراج يغادرونها بعد الهزة', thumbnailSrc:'../images/thumbnails/kqmIrPec2WAjpg.jpg'}, 
+		{ videoTitle:'اعنف زلزال يضرب ايران منذ 40عاما', thumbnailSrc:'../images/thumbnails/PLtRrWkB094.jpg'}, 
+		{ videoTitle:'زلزال يضرب قارة اسيا بالكامل', thumbnailSrc:'../images/thumbnails/wfNbO291NO0.jpg'}, 
+		{ videoTitle:'31 قتيلا على الأقل ومئات الجرحى في زلزال يضرب جنوب إيران', thumbnailSrc:'../images/thumbnails/CYVK_mKYV0U.jpg'}, 
+		{ videoTitle:'زلزال بقوة 6.3 قرب بوشهر في ايران يشعر به سكان دولة الإمارات', thumbnailSrc:'../'}, 
+		{ videoTitle:'IRANS EARTHQUAKES AND NUCLEAR REACTORS LAUNCHING!', thumbnailSrc:'../images/thumbnails/9NWJ-Hz_3zw.jpg'}, 
+		{ videoTitle:'زلزال يضرب إيران ويهز دول الخليج', thumbnailSrc:'../images/thumbnails/EcHP5-pW_4M.jpg'}, 
+		{ videoTitle:'Iran Earthquake (Magnitude 7,8) Aftermath Footage - April 16, 2013', thumbnailSrc:'../images/thumbnails/0WrcBhKLtes.jpg'}, 
+		{ videoTitle:'الإمارات.. لا خسائر بعد الزلزال', thumbnailSrc:'../images/thumbnails/PLtRrWkB094.jpg'}, 
+		{ videoTitle:'Mega Earthquake Shakes Iran ', thumbnailSrc:'../images/thumbnails/EcHP5-pW_4M.jpg'}] ;
 
 
 
